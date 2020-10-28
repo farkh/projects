7be81f13526e08cb67ee16bde0dd17bebb62d752
Ainur Farkh <farkhutdinov139@gmail.com>
2020-10-28 21:40:01 +0300

    Fix static server

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const path = require('path')
 const expressApp = express()
 const PORT = process.env.PORT || 8080
 
-expressApp.use('/', express.static(path.join(__dirname, 'public')))
+expressApp.use(express.static(__dirname));
+expressApp.use(express.static(path.join(__dirname, 'build')));
+
+expressApp.use('/ping', (_, res) => res.send('pong'))
+expressApp.get('/*', function (_, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 const startApp = () => {
     expressApp.listen(PORT, console.log(`Client static server up and running at port ${PORT}`))
